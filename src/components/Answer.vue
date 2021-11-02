93bc06335517cf5f594816732287a3e3efe40153
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-11-02 16:38:44 +0100

    3rd commit question random

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Answer
+  <div >
+    <div v-for="(risp,index) in risposte" :key="index">
+    {{risp.risposta}} 
+    </div>
   </div>
 </template>
 
@@ -12,7 +14,63 @@ export default {
   },
   data() {
         return {
-           answers:["1- Come ti chiami?", "2- 2+2?", "3- quanti anni hai?"] 
+          risposte: [
+            {
+              risposta: { a: "2", b: "5", c: "7", d: "10" },
+              rispostaCorretta: "7",
+            },
+            {
+              risposta: { a: "Bucarest", b: "Milano", c: "Berlino", d: "Oslo" },
+              rispostaCorretta: "Bucarest",
+            },
+            {
+              risposta: {
+                a: "Cristoforo Colombo",
+                b: "Carlo Pellegatti",
+                c: "216",
+                d: "Amerigo Vespucci",
+              },
+              rispostaCorretta: "Cristoforo Colombo",
+            },
+            {
+              risposta: { a: "1993", b: "1910", c: "1915", d: "1879" },
+              rispostaCorretta: "1915",
+            },
+            {
+              risposta: {
+                a: "Zlatan",
+                b: "Er Freddo",
+                c: "Otello Lupacchini",
+                d: "Freddy Mercury",
+              },
+              rispostaCorretta: "Freddy Mercury",
+            },
+            {
+              risposta: { a: "10°", b: "5°", c: "70°", d: "100°" },
+              rispostaCorretta: "100°",
+            },
+            {
+              risposta: { a: "Spagna", b: "Germania", c: "Italia", d: "Danimarca" },
+              rispostaCorretta: "Italia",
+            },
+            {
+              risposta: {
+                a: "let item = 23",
+                b: "function getItem() ...",
+                c: "return",
+                d: "string.join()",
+              },
+              rispostaCorretta: "function getItem() ...",
+            },
+            {
+              risposta: { a: "Props", b: "Emit", c: "Componenti", d: "Altro" },
+              rispostaCorretta: "Props",
+            },
+            {
+              risposta: { a: "2", b: "5", c: "7", d: "10" },
+              rispostaCorretta: "2",
+            },
+          ],
         }
     },
 }
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-      {{questions}}
+  <div >
+    <div v-for="(dom,index) in domandaRandom" :key="index">
+      {{dom}}
+    </div>
   </div>
 </template>
 
@@ -12,7 +14,66 @@ export default {
   },
   data() {
         return {
-           questions:["1- Come ti chiami?", "2- 2+2?", "3- quanti anni hai?"] 
+          domandaRandom: [],
+    domande: [
+            {
+              domanda: "Quante champions ha vinto il Milan?",
+              id: 1
+            },
+            {
+              domanda: "La capitale della Romania?",
+              id: 2
+            },
+            {
+              domanda: "Chi ha scoperto l'America?",
+              id: 3
+            },
+            {
+              domanda: "Quando è iniziata la prima guerra mondiale?",
+              id: 4
+            },
+            {
+              domanda: "Chi era il frontman dei Queen?",
+              id: 5
+            },
+            {
+              domanda: "A quale temperatura bolle l'acqua?",
+              id: 6
+            },
+            {
+              domanda: "Chi ha vinto gli europei nel 2021?",
+              id: 7
+            },
+            {
+              domanda: "Quale di queste è una funzione?",
+              id: 8
+            },
+            {
+              domanda: "Come si passano dati ad un componente figlio in Vue?",
+              id: 9
+            },
+            {
+              domanda: "Quante champions ha vinto la Juve?",
+              id: 10
+            },
+          ], 
+        }
+    },
+    mounted(){
+      this.nuovaDomanda()
+    },
+    methods:{
+        getRandomDomanda(max) {
+        return Math.floor(Math.random() * (max-1)+1)
+        },
+        
+        nuovaDomanda(){
+          let numeroRandom= this.getRandomDomanda(this.domande.length)
+          this.domande.forEach(el=>{
+            if(el.id == numeroRandom){
+              this.domandaRandom.push(el.domanda)
+            }
+          })
         }
     },
 }
